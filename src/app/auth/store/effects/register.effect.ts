9befa0a9c3ca89555652161a2e6461b6a67f4d6c
Zhorabaevv <nzhorabaev777@gmail.com>
2023-01-10 03:50:30 +0600

    add selectors and register effect

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -3,25 +3,32 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { switchMap, map, catchError } from 'rxjs/operators';
+import { switchMap, map, catchError, tap } from 'rxjs/operators';
 
 import { AuthService } from 'src/app/auth/services/auth.service';
-import { registerAction, registerFailedAction, registerSuccessAction } from 'src/app/auth/store/actions/register.action';
+import {
+  registerAction,
+  registerFailedAction,
+  registerSuccessAction,
+} from 'src/app/auth/store/actions/register.action';
+import { PersistanceService } from 'src/app/shared/services/persistance.service';
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 
 @Injectable()
-
 export class RegisterEffect {
-  register$ = createEffect(() => this.actions$.pipe(
+  register$ = createEffect(() =>
+    this.actions$.pipe(
       ofType(registerAction),
       switchMap(({ request }) => {
         return this.authService.register(request).pipe(
           map((currentUser: CurrentUserInterface) => {
+            // window.localStorage.setItem('accessToken', currentUser.token);
+            this.persistanService.set('accessToken', currentUser.token);
             return registerSuccessAction({ currentUser });
           }),
           catchError((errorResponse: HttpErrorResponse) => {
             return of(
-              registerFailedAction({ errors: errorResponse.error.errors})
+              registerFailedAction({ errors: errorResponse.error.errors })
             );
           })
         );
@@ -29,10 +36,21 @@ export class RegisterEffect {
     )
   );
 
+  redirectAfterSubmit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(registerSuccessAction),
+        tap(() => {
+          this.router.navigateByUrl('/');
+        })
+      ),
+    { dispatch: false }
+  );
 
   constructor(
     private actions$: Actions,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private persistanService: PersistanceService
   ) {}
 }
